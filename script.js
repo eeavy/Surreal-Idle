0eacf82211ce612c7e869913854bea202b593e99
eeavy <79663298+eeavy@users.noreply.github.com>
2022-09-24 23:50:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
 let town = {
-      gold: 0,
-      wood: 0,
-      stone: 0,
-      clay: 0,
-      men: 0,
+ gold: 0,
+ wood: 0,
+ stone: 0, 
+ clay: 0,
+ men: 0,
+ 
+  mall: { 
+   time: 5,
+  }, 
+ }
 
-      monument: {
-            time: 5
-      },
+ 
+function update() { 
+	$(document).ready(function(){ 
+	$(".gold").html(`<img src="src/gold.png">`+ "Gold: " + town.gold);
+$("#mall").html("Mall " + town.mall.time) 	;
+ 	}) 
+		setTimeout(update, 100);
+	}
+ update(); 
 
+
+function canBuild(e) {
+if(town[e].time>0){
+    setTimeout(build(e), 1000)
+}
+}
+function build(e){
+    if(town[e].time>0){
+    town[e].time--
+}
+    console.log(e)
 }
 
-function update() {
-	$( document ).ready(function(){
-      $(".gold").html(`<img src="src/gold.png">`+ "Gold: " + town.gold);
-      $(".wood").html("Wood: " + town.wood);
-      $(".stone").html("Stone: " + town.stone);
-      $(".clay").html("Clay: " + town.clay);
-      $(".men").html("Men: " + town.men);
 
-      $(".monument .time").html(town.monument.time);
-
-
-	})
-	setTimeout(update, 1000);
+function openNav() {
+  document.getElementById("mySidenav").style.display = "block";
 }
-update();
 
-function build(x) {
-      if (x.time>0) {
-            x.time --
-            setTimeout(build, 1000, x)
-      }
-      else{
-            built(x)
-      }
-}
-function built(x){
-      switch(x){
-            case town.monument:
-            town.gold = 10;
-            break;
-      }
+function closeNav() {
+  document.getElementById("mySidenav").style.display = "none";
 }
